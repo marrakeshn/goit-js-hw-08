cca68aa85ffbe9e5a10982ad306df1d52c74f822
marrakeshn <nesveteymark@gmail.com>
2023-02-17 03:13:55 +0200

    done task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
+// Add imports above this line
 // Change code below this line
+let instance;
+const galleryContainerItem = document.querySelector(".gallery");
+const itemsMarkup = createItemsMarkup(galleryItems);
+galleryContainerItem.insertAdjacentHTML("beforeend",  itemsMarkup);
 
-console.log(galleryItems);
+function createItemsMarkup(item) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+                <a class="gallery__link" href="${original.value}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`;
+        })
+        .join("");
+}
+
+const onContainerClick = (e) => {
+    e.preventDefault();
+
+    if (e.target.classList.contains("gallery")) return;
+        const source = e.target.dataset.source;
+        
+    instance = basicLightbox.create(`
+        <img src="${source}"width="800" height="600">`);
+        
+    instance.show();
+};
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    instance.close();
+  }
+});
+
+
+
+
+galleryContainerItem.addEventListener("click", onContainerClick);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('input'),
+};
+const formData = {};
+
+populateTextarea();
